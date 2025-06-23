a3b7074f0eea07f22b9f52d3e6728a86977711b1
walid <walid.benaissa@outlook.fr>
2025-06-23 11:53:07 +0200

    preparation de la page d'acceuil

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <h1>Page d'accueil</h1>
+    <p>Bienvenue dans notre application <strong>PHP</strong></p>
+</body>
+</html>
